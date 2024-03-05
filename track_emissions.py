0245d9fcd1fecaa3cfaad7d0dad4b0a2094a1d62
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-03-05 16:18:41 +0530

    Update track_emissions.py

--- track_emissions.py
+++ track_emissions.py
@@ -45,7 +45,7 @@ with open(csv_file, 'w', newline='') as file:
                 # Stop tracking
 # Stop tracking
                 tracker.stop()
-                emissions_data = tracker.emissions 
+                emissions_data = tracker._emissions 
                 writer.writerow([filename, emissions_data.emissions, emissions_data.energy_consumed, emissions_data.run_time, emissions_data.cpu_power, emissions_data.ram_power, emissions_data.gpu_power]) # Use dot notation
 
 print("Emissions data written to emissions_data.csv")
