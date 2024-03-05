9849ceff68cb5fa3c8915273a13bcca9186bf54e
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-03-05 13:35:29 +0530

    Update track_emissions.py

--- track_emissions.py
+++ track_emissions.py
@@ -50,12 +50,11 @@ with open(csv_file, 'w', newline='') as file:
 # Get the emissions data
                 emissions_data = tracker._emissions
 
-# Get additional data
                 timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                duration = emissions_data['duration']
-                cpu_power = emissions_data['cpu_power']
-                ram_power = emissions_data['ram_power']
-                energy_consumed = emissions_data['energy_consumed']
+                duration = emissions_data.duration # Use dot notation
+                cpu_power = emissions_data.cpu_power # Use dot notation
+                ram_power = emissions_data.ram_power # Use dot notation
+                energy_consumed = emissions_data.energy_consumed # Use dot notation
 
 # Write emissions data to CSV
                 writer.writerow([filename, timestamp, emissions_data['emissions'], duration, cpu_power, ram_power, energy_consumed])
