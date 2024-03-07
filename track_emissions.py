349804d360d402a58d13a8cc9fe7c6d5605f4cc1
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-03-07 13:32:56 +0530

    Update track_emissions.py write mode

--- track_emissions.py
+++ track_emissions.py
@@ -9,11 +9,6 @@ import pandas as pd
 # Directory containing the scripts
 scripts_dir = r"C:\ProgramData\Jenkins\.jenkins\workspace\GreenCodeScanPipeline"
 
-# Create a CSV file to store emissions data
-with open('emissions_data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Filename", "Timestamp", "Emissions (kgCO2)", "Duration", "CPU Power", "RAM Power", "Energy Consumed", "Test Results"])
-
 # Iterate over each script in the directory
 for script in os.listdir(scripts_dir):
     if script.endswith('.py'):
@@ -37,32 +32,25 @@ for script in os.listdir(scripts_dir):
         # Stop the emissions tracker
         tracker.stop()
 
-# Read the emissions data from the CSV file
         # Read the emissions data from the CSV file
-        emissions_data = pd.read_csv('C:/ProgramData/Jenkins/.jenkins/workspace/GreenCodeScanPipeline/emissions.csv')
+        emissions_data = pd.read_csv('C:/ProgramData/Jenkins/.jenkins/workspace/GreenCodeScanPipeline/emissions.csv').iloc[-1]
 
-# Check if the DataFrame is empty
-        if emissions_data.empty:
-            print(f"No emissions data generated for script {script}.")
-        else:
-    # Retrieve the last row of emissions data
-            emissions_data = emissions_data.iloc[-1]
-
-    # Retrieve and format the emissions data
+        # Retrieve and format the emissions data
         data = [
-                script,
-                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                emissions_data['emissions'],
-                duration,
-                emissions_data['cpu_power'],
-                emissions_data['ram_power'],
-                emissions_data['energy_consumed']
-            ]
-        print(data)
-    # Write the data to the CSV file
-        with open('emissions_data.csv', 'a', newline='') as file:
+            script,
+            datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            emissions_data['emissions'],
+            duration,
+            emissions_data['cpu_power'],
+            emissions_data['ram_power'],
+            emissions_data['energy_consumed']
+        ]
+
+        # Write the data to the CSV file
+        with open('emissions_data.csv', 'w', newline='') as file:
             writer = csv.writer(file)
+            writer.writerow(["Filename", "Timestamp", "Emissions (kgCO2)", "Duration", "CPU Power", "RAM Power", "Energy Consumed", "Test Results"])
             writer.writerow(data)
             file.flush()
-            
+
 print("Emissions data written to emissions_data.csv")
