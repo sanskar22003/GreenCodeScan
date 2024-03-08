91a10c915d8d81e06144c235c13862d6913ecf1c
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-03-08 13:01:52 +0530

    Update Java_sample.java

--- Java_sample.java
+++ Java_sample.java
@@ -1,13 +1,18 @@
 class Student3{
-int id;
-String name;
+    int id;
+    String name;
 
-void display(){System.out.println(id+" "+name);}
+    String display(){ return id + " " + name; }
 
-public static void main(String args[]){
-Student3 s1=new Student3();
-Student3 s2=new Student3();
-s1.display();
-s2.display();
-}
+    public static void main(String args[]){
+        Student3 s1 = new Student3();
+        s1.id = 1;
+        s1.name = "John";
+        System.out.println(s1.display());
+
+        Student3 s2 = new Student3();
+        s2.id = 2;
+        s2.name = "Jane";
+        System.out.println(s2.display());
+    }
 }
