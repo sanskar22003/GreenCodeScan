bb0116dc5736b3b795adf41ae8a21a53cbf41628
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-03-15 17:11:37 +0530

    Update update_google_sheets.py

--- update_google_sheets.py
+++ update_google_sheets.py
@@ -5,7 +5,7 @@ import csv
 # Use the credentials file you downloaded when setting up the Google Sheets API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('C:\Users\sansk\Downloads\evocative-shore-417308-e617b5ec9771.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\sansk\Downloads\evocative-shore-417308-e617b5ec9771.json', scope)
 client = gspread.authorize(creds)
 
 # Open the Google Sheets document and get the first sheet
@@ -13,7 +13,7 @@ sheet1 = client.open("Dynamic_Code_Analysis").get_worksheet(0)
 sheet2 = client.open("Server_Tracking_emissions").get_worksheet(1)
 
 # Read the CSV files
-csv_files = ['C:\ProgramData\Jenkins\.jenkins\workspace\GreenCodeScanPipeline\emissions_data.csv', 'C:\ProgramData\Jenkins\.jenkins\workspace\GreenCodeScanPipeline\server_data.xlsx']
+csv_files = [r'C:\ProgramData\Jenkins\.jenkins\workspace\GreenCodeScanPipeline\emissions_data.csv', r'C:\ProgramData\Jenkins\.jenkins\workspace\GreenCodeScanPipeline\server_data.xlsx']
 sheets = [sheet1, sheet2]
 
 for csv_file, sheet in zip(csv_files, sheets):
