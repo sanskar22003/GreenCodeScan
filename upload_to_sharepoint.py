8880737877b427eb7f9f77a992a5e816810f2f18
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-03-21 13:54:55 +0530

    Create upload_to_sharepoint.py

---
+++ upload_to_sharepoint.py
@@ -0,0 +1,18 @@
+from shareplum import Site
+from shareplum import Office365
+
+# Authenticate to Office365
+authcookie = Office365('https://sbupune.sharepoint.com', username='SANSKAR.MCA22003@SBUP.EDU.IN', password='Sk@6353910033').GetCookies()
+
+# Initialize the Site
+site = Site('https://sbupune.sharepoint.com/sites/TechMahindraGreenCodePipeline2', authcookie=authcookie)
+
+# Get the folder
+folder = site.Folder('Shared Documents/Pipeline CSVs')
+
+# Upload the files
+with open('C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GreenCodeScanPipeline\\emissions_data.csv', 'rb') as file:
+    folder.upload_file(file, 'emissions_data.csv')
+
+with open('C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\GreenCodeScanPipeline\\server_data.xlsx', 'rb') as file:
+    folder.upload_file(file, 'server_data.xlsx')
