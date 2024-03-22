4948aa1564c1e3f2f018f19a82eb8530f91fc6df
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-03-22 10:52:05 +0530

    Update and rename upload_to_sharepoint.py to upload_to_sharepoint.ps1

---
+++ upload_to_sharepoint.ps1
@@ -0,0 +1,39 @@
+# Import the SharePoint client libraries
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+
+# Set the variables
+$siteUrl = "https://sbupune.sharepoint.com/sites/TechMahindraGreenCodePipeline2"
+$listName = "Shared Documents"
+$folderName = "Pipeline CSVs"
+$username = "SANSKAR.MCA22003@SBUP.EDU.IN"
+$password = ConvertTo-SecureString "Sk@6353910033" -AsPlainText -Force
+
+# Create a credential object
+$credential = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $password)
+
+# Connect to the SharePoint site
+$context = New-Object Microsoft.SharePoint.Client.ClientContext($siteUrl)
+$context.Credentials = $credential
+
+# Get the list and folder
+$list = $context.Web.Lists.GetByTitle($listName)
+$folder = $list.RootFolder.Folders.GetByUrl($folderName)
+$context.Load($list)
+$context.Load($folder)
+$context.ExecuteQuery()
+
+# Upload the files
+$files = @("C:\ProgramData\Jenkins\.jenkins\workspace\GreenCodeScanPipeline\emissions_data.csv", "C:\ProgramData\Jenkins\.jenkins\workspace\GreenCodeScanPipeline\server_data.xlsx")
+foreach ($filePath in $files) {
+    $fileContent = [System.IO.File]::ReadAllBytes($filePath)
+    $fileInfo = New-Object Microsoft.SharePoint.Client.FileCreationInformation
+    $fileInfo.Content = $fileContent
+    $fileInfo.Url = [System.IO.Path]::GetFileName($filePath)
+    $fileInfo.Overwrite = $true
+    $file = $folder.Files.Add($fileInfo)
+    $context.Load($file)
+    $context.ExecuteQuery()
+
+    Write-Host "File uploaded successfully: $filePath"
+}
