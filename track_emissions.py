48b098170fc382ca6f7a65c8c3955bf1f874770d
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-07-14 21:18:08 +0530

    Update track_emissions.py

--- track_emissions.py
+++ track_emissions.py
@@ -13,8 +13,6 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path=".env", verbose=True, override=True)
 
 def process_folder(BASE_DIR, EMISSIONS_DATA_CSV, RESULT_DIR, suffix):
-    SCRIPTS_DIR = os.path.join(BASE_DIR)
-    TESTS_DIR = os.path.join(BASE_DIR, "tests")
     PYTEST_PATH = os.getenv('PYTEST_PATH')
     MAVEN_PATH = os.getenv('MAVEN_PATH')
     CUSTOMER_NAME = "ZF"
@@ -32,69 +30,73 @@ def process_folder(BASE_DIR, EMISSIONS_DATA_CSV, RESULT_DIR, suffix):
             writer = csv.writer(file)
             writer.writerow(["Customer Name", "Application name", "File Type", "Timestamp", "Emissions (gCO2eq)", "Duration", "emissions_rate", "CPU Power (KWh)", "GPU Power (KWh)", "RAM Power (KWh)", "CPU Energy (Wh)", "GPU Energy (KWh)", "RAM Energy (Wh)", "Energy Consumed (Wh)", "Test Results"])
 
-    for script in os.listdir(SCRIPTS_DIR):
-        if script.endswith(('.py', '.java', '.cpp', '.cs')) and script != 'track_emissions.py' and script != 'product_detailsTest.java' and script != 'server_emissions.py' and script != 'update_google_sheets.py':
-            tracker = EmissionsTracker()
-            duration = None
-            _, file_type = os.path.splitext(script)
-            test_script = os.path.join(TESTS_DIR if script.endswith('.py') else SCRIPTS_DIR, os.path.splitext(script)[0] + 'Test')
-            if os.path.exists(test_script + '.py') or os.path.exists(test_script + '.java'):
-                if script.endswith('.py'):
-                    sys.path.append(SCRIPTS_DIR)
-                    test_result = subprocess.run([PYTEST_PATH, test_script + '.py'], capture_output=True, text=True)
-                elif script.endswith('.java'):
-                    os.chdir(SCRIPTS_DIR)
-                    test_result = subprocess.run([MAVEN_PATH, '-Dtest=' + os.path.splitext(script)[0] + 'Test', 'test'], capture_output=True, text=True)
-                test_output = 'Pass' if test_result.returncode == 0 else 'Fail'
-            else:
-                test_output = 'No tests found for script.'
+    # Recursively find files with specific extensions
+    for root, dirs, files in os.walk(BASE_DIR):
+        for script in files:
+            if script.endswith(('.py', '.java', '.cpp', '.cs')) and script != 'track_emissions.py' and script != 'product_detailsTest.java' and script != 'server_emissions.py' and script != 'update_google_sheets.py':
+                script_path = os.path.join(root, script)
+                tracker = EmissionsTracker()
+                duration = None
+                _, file_type = os.path.splitext(script)
+                test_script = os.path.join(root, "tests" if script.endswith('.py') else "", os.path.splitext(script)[0] + 'Test')
 
-            tracker.start()
+                if os.path.exists(test_script + '.py') or os.path.exists(test_script + '.java'):
+                    if script.endswith('.py'):
+                        sys.path.append(root)
+                        test_result = subprocess.run([PYTEST_PATH, test_script + '.py'], capture_output=True, text=True)
+                    elif script.endswith('.java'):
+                        os.chdir(root)
+                        test_result = subprocess.run([MAVEN_PATH, '-Dtest=' + os.path.splitext(script)[0] + 'Test', 'test'], capture_output=True, text=True)
+                    test_output = 'Pass' if test_result.returncode == 0 else 'Fail'
+                else:
+                    test_output = 'No tests found for script.'
 
-            try:
-                start_time = time.time()
-                if script.endswith('.py'):
-                    subprocess.run(['python', os.path.join(SCRIPTS_DIR, script)], timeout=60)
-                elif script.endswith('.java'):
-                    subprocess.run(['javac', os.path.join(SCRIPTS_DIR, script)], timeout=60)
-                    subprocess.run(['java', '-cp', SCRIPTS_DIR, os.path.splitext(script)[0]], timeout=60)
-                duration = time.time() - start_time
-            except subprocess.TimeoutExpired:
-                print(f"Script {script} exceeded the timeout limit.")
+                tracker.start()
 
-            tracker.stop()
+                try:
+                    start_time = time.time()
+                    if script.endswith('.py'):
+                        subprocess.run(['python', script_path], timeout=60)
+                    elif script.endswith('.java'):
+                        subprocess.run(['javac', script_path], timeout=60)
+                        subprocess.run(['java', '-cp', root, os.path.splitext(script)[0]], timeout=60)
+                    duration = time.time() - start_time
+                except subprocess.TimeoutExpired:
+                    print(f"Script {script} exceeded the timeout limit.")
 
-            emissions_csv_default_path = 'emissions.csv'  # Default path where codecarbon saves the file
-            emissions_csv_target_path = EMISSIONS_CSV  # Adjusted target path within 'result' directory with suffix
+                tracker.stop()
 
-            # Check if the file exists at the default location and move it
-            if os.path.exists(emissions_csv_default_path):
-                shutil.move(emissions_csv_default_path, emissions_csv_target_path)
+                emissions_csv_default_path = 'emissions.csv'  # Default path where codecarbon saves the file
+                emissions_csv_target_path = EMISSIONS_CSV  # Adjusted target path within 'result' directory with suffix
 
-            if os.stat(EMISSIONS_CSV).st_size != 0:
-                emissions_data = pd.read_csv(EMISSIONS_CSV).iloc[-1]
-                data = [
-                    CUSTOMER_NAME,
-                    script,
-                    file_type,
-                    datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                    emissions_data['emissions'] * 1000,
-                    duration,
-                    emissions_data['emissions_rate'] * 1000,
-                    emissions_data['cpu_power'],
-                    emissions_data['gpu_power'],
-                    emissions_data['ram_power'],
-                    emissions_data['cpu_energy'] * 1000,
-                    emissions_data['gpu_energy'],
-                    emissions_data['ram_energy'] * 1000,
-                    emissions_data['energy_consumed'] * 1000,
-                    test_output
-                ]
+                # Check if the file exists at the default location and move it
+                if os.path.exists(emissions_csv_default_path):
+                    shutil.move(emissions_csv_default_path, emissions_csv_target_path)
 
-                with open(EMISSIONS_DATA_CSV, 'a', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(data)
-                    file.flush()
+                if os.stat(EMISSIONS_CSV).st_size != 0:
+                    emissions_data = pd.read_csv(EMISSIONS_CSV).iloc[-1]
+                    data = [
+                        CUSTOMER_NAME,
+                        script,
+                        file_type,
+                        datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                        emissions_data['emissions'] * 1000,
+                        duration,
+                        emissions_data['emissions_rate'] * 1000,
+                        emissions_data['cpu_power'],
+                        emissions_data['gpu_power'],
+                        emissions_data['ram_power'],
+                        emissions_data['cpu_energy'] * 1000,
+                        emissions_data['gpu_energy'],
+                        emissions_data['ram_energy'] * 1000,
+                        emissions_data['energy_consumed'] * 1000,
+                        test_output
+                    ]
+
+                    with open(EMISSIONS_DATA_CSV, 'a', newline='') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(data)
+                        file.flush()
 
     print(f"Emissions data and test results written to {EMISSIONS_DATA_CSV}")
 
@@ -104,5 +106,5 @@ green_refined_directory = os.getenv('GREEN_REFINED_DIRECTORY')
 result_dir = os.getenv('RESULT_DIR')
 
 # Process each folder with suffix
-process_folder(source_directory, os.path.join(result_dir, 'main_before_emissions_data.csv'), result_dir, 'before_in-detail')
+process_folder(source_directory, os.path.join(result_dir, 'main_before_emissions_data.csv'), result_dir, 'before-in-detail')
 process_folder(green_refined_directory, os.path.join(result_dir, 'main_after_emissions_data.csv'), result_dir, 'after-in-detail')
