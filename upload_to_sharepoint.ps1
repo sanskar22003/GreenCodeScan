4a6c71a23d84608a21c56f33b258c5a699852374
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-07-15 19:23:59 +0530

    Update upload_to_sharepoint.ps1

--- upload_to_sharepoint.ps1
+++ upload_to_sharepoint.ps1
@@ -1,5 +1,5 @@
 # Load environment variables from .env file
-$envPath = ".\path\to\your\.env"
+$envPath = "C:\ProgramData\Jenkins\.jenkins\workspace\GreenCodeScanPipeline\.env"
 if (Test-Path $envPath) {
     $env = Get-Content $envPath | ForEach-Object {
         if ($_ -match '^([^=]+)=(.*)$') {
