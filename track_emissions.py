37fe48d52d5324d6f06fad69f8c89375c68a6ddf
sanskar vegad <147686799+sanskar22003@users.noreply.github.com>
2024-12-23 11:34:34 +0530

    Update track_emissions.py

--- track_emissions.py
+++ track_emissions.py
@@ -589,8 +589,8 @@ def generate_html_report(result_dir):
         xaxis_title="Date",
         yaxis_title="Value",
         xaxis_type="category",
-        width=800,
-        height=400,
+        width=1200,
+        height=500,
     )
 
     # Save the chart as a Plotly HTML div
